0894f70ade91b9a0c96d23006fe9e5a13f7a634a
tabbzi <kathleen.m.morrill@gmail.com>
2021-06-24 15:37:48 +0000

    Commented and used new ancestry file for Adeleke's workflow

--- bin/DEseqAncestryStratification.R
+++ bin/DEseqAncestryStratification.R
@@ -4,11 +4,12 @@ require(tidyverse)
 require(readr)
 require(data.table)
 require(TCGAbiolinks)
-require(DESeq2)
+#require(DESeq2)
+require(EDAseq)
 
 # Settings
 # local: setwd("~/scratch/Cancer-Health-Disparities-through-genomic-epidemiology-Lens/")
-setwd("~/Cancer-Health-Disparities-through-genomic-epidemiology-Lens/")
+setwd("~/branch/Cancer-Health-Disparities-through-genomic-epidemiology-Lens/")
 
 # Cancer Type
 typeCancer = "COAD"
@@ -16,21 +17,110 @@ typeTCGA = paste("TCGA", typeCancer, sep ="-")
 
 # Import Data
 # import clinical test gene sets:
-clinicalTests = read.delim(file = ".",
-)
+clinicalTests = read.delim(file = "./data/gene_sets/OncotypeDX_Colon_RS.gmx.tsv",
+                           sep = "\t",
+                           header = T) %>% as.data.table()
 
-# import inferred ancestry in TCGA participants
+# import inferred ancestry in TCGA participants:
 ancestryTCGA = read.delim(file = "./data/anc/TCGA_consensus_ancestry.tsv",
                           sep = "\t",
                           header = T) %>% as.data.table()
 
-# import 
+# import TCGA data
 query <- GDCquery(
   project = typeTCGA,
-  data.category = "Transcriptome Profiling", 
-  data.type = "Gene Expression Quantification",
-  workflow.type = "HTSeq - Counts"
-)
+  data.category = "Gene expression",
+  data.type = "Gene expression quantification",
+  platform = "Illumina HiSeq", 
+  file.type  = "results", 
+  experimental.strategy = "RNA-Seq",
+  sample.type = c("Primary Tumor"),
+  legacy = TRUE)
 
 GDCdownload(query)
-data <- GDCprepare(query)
+data <- GDCprepare(query, 
+                   summarizedExperiment = TRUE)
+
+# bin participants by ancestry:
+AFR = ancestryTCGA[tumor_type == typeCancer & consensus_ancestry %in% c("afr_admix","afr")]$patient
+EUR = ancestryTCGA[tumor_type == typeCancer & consensus_ancestry %in% c("eur_admix","eur")]$patient
+
+# identify samples by participants:
+AFRcols = unlist(sapply(AFR, FUN = function(x) colnames(data)[colnames(data) %like% x]))
+EURcols = unlist(sapply(EUR, FUN = function(x) colnames(data)[colnames(data) %like% x]))
+
+# stratify data by ancestry:
+queryAFR <- GDCquery(
+  project = typeTCGA,
+  barcode = AFRcols,
+  data.category = "Gene expression",
+  data.type = "Gene expression quantification",
+  platform = "Illumina HiSeq", 
+  file.type  = "results", 
+  experimental.strategy = "RNA-Seq",
+  sample.type = c("Primary Tumor"),
+  legacy = TRUE)
+GDCdownload(queryAFR)
+dataAFR <- GDCprepare(queryAFR, 
+                   summarizedExperiment = TRUE)
+
+queryEUR <- GDCquery(
+  project = typeTCGA,
+  barcode = EURcols,
+  data.category = "Gene expression",
+  data.type = "Gene expression quantification",
+  platform = "Illumina HiSeq", 
+  file.type  = "results", 
+  experimental.strategy = "RNA-Seq",
+  sample.type = c("Primary Tumor"),
+  legacy = TRUE)
+GDCdownload(queryEUR)
+dataEUR <- GDCprepare(queryEUR, 
+                   summarizedExperiment = TRUE)
+
+# data prep:
+dataPrepEUR <- TCGAanalyze_Preprocessing(
+  object = dataEUR,
+  cor.cut = 0.6,    
+  datatype = "raw_count",
+  filename = "coad_EUR.png")
+
+dataPrepAFR <- TCGAanalyze_Preprocessing(
+  object = dataAFR,
+  cor.cut = 0.6,    
+  datatype = "raw_count",
+  filename = "coad_AFR.png")
+
+# differential gene expression:
+dataNormEUR <- TCGAanalyze_Normalization(
+  tabDF = dataPrepEUR,
+  geneInfo = TCGAbiolinks::geneInfo,
+  method = "gcContent")
+
+dataNormAFR <- TCGAanalyze_Normalization(
+  tabDF = dataPrepAFR,
+  geneInfo = TCGAbiolinks::geneInfo,
+  method = "gcContent")
+
+
+
+dataDEGs <- TCGAanalyze_DEA(
+  mat1 = dataNormEE,
+  mat2 = dataNormBB,
+  Cond1type = "Whitw",
+  Cond2type = "Black",
+  fdr.cut = 0.01 ,
+  logFC.cut = 1,
+  method = "glmLRT")
+
+# Number of differentially expressed genes (DEG)
+nrow(dataDEGs)
+
+
+#dataDEGs that showed logFC > 1 and FDR < 0.01 for example, can be considered significantly Up-regulated genes in cancer (EE) tissues. 
+
+
+# DEGs table with expression values in normal and tumor samples
+dataDEGsFiltLevel <- TCGAanalyze_LevelTab(dataDEGs,"Tumor","Normal",
+                                          dataNormEE, dataNormBB)
+
